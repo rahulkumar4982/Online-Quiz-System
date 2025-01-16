cd19bcf446e36eb189ede8f50bc6d2df9ea05620
Rahul Kumar <111380343+rahulkumar4982@users.noreply.github.com>
2025-01-16 12:08:53 +0530

    Create Option.java

---
+++ Option.java
@@ -0,0 +1,17 @@
+import javax.persistence.*;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    // Getters and Setters
+}
